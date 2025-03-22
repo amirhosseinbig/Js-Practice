72a7ce8b3f4ea37bc5507cbdad1bd298f9341497
Amir Bigdeli <mygithubacount@gmail.com>
2025-03-22 18:06:32 +0330

    reduce method

--- 1.js
+++ 1.js
@@ -299,3 +299,9 @@ let resultevery = list.every(x => x > 0); //false
 let resultSome = list.some(x => x > 10); //true
 
 console.log(resultSome)
+
+let numList = [1,2,3,4,5];
+
+let sum = numList.reduce((a,c) => console.log(a,c) , 0); //=> 15 , the second argument is the initial value of sum = 0;
+
+console.log(sum);
